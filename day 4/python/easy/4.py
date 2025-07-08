f346fe6d60fba301a6857671a28724fad6685b20
HarishankarVj12 <116081867+HarishankarVj12@users.noreply.github.com>
2025-07-08 22:58:13 +0530

    day 4 medium task python completed

--- day 4/python/easy/4.py
+++ day 4/python/easy/4.py
@@ -1,15 +1,11 @@
 ## Task:
 ## Write a Python program that: Accepts a string, Counts the number of vowels (a, e, i, o, u) in it
+from collections import Counter
 def main():
-    user_word = input("Enter the input word: ")
-    vowels = {'a', 'e', 'i', 'o', 'u'}  # Using a set for efficient lookup
-    count = 0
-
-    for char in user_word.lower():
-        if char in vowels:
-            count += 1
-
-    print(f"Number of vowels in the word: {count}")
+    user_word = input("Enter the input word: ").lower()
+    vowels = {'a', 'e', 'i', 'o', 'u'}
+    counter = Counter(ch for ch in user_word if ch in vowels)
+    print("Vowel counts:", dict(counter))
 
 if __name__ == "__main__":
     main()
