25f32dafb1a59d7af243386c732f4f59a5055022
HarishankarVj12 <116081867+HarishankarVj12@users.noreply.github.com>
2025-07-12 16:59:39 +0530

    update the day 8 python medium task

---
+++ day 8/python/medium/8.py
@@ -0,0 +1,22 @@
+## Task
+## Task: Accept a sentence, and return the word with the highest number of vowels.
+
+def main():
+    sent=input("Enter the sentence input: ").lower()
+    vowels=[ 'a', 'e', 'i', 'o', 'u']
+    sentence=sent.split()
+    max_vowel_word=""
+    max_vowel_count=0
+    for se in sentence:
+        count = 0
+        for word in se:
+            if word in vowels:
+                count +=1
+        if max_vowel_count < count:
+            max_vowel_count = count
+            max_vowel_word = se
+
+    print(f"Word with highest vowels: {max_vowel_word} ({max_vowel_count} vowels)")
+
+if __name__ == "__main__":
+    main()
