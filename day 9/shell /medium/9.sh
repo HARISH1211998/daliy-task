164b0098a9c27d05e8d5da61ff763c49eaba436a
HarishankarVj12 <116081867+HarishankarVj12@users.noreply.github.com>
2025-08-22 09:09:42 +0530

    update the shell medium task

--- day 9/shell /medium/9.sh
+++ day 9/shell /medium/9.sh
@@ -1,7 +1,6 @@
 ## Task: Accept a directory path. Recursively find:
 ## All .log files larger than 1MB, Print their path and size, Hint: Use find, -size +1M, -name "*.log"
-
-#! /bin/bash 
+ 
 #!/bin/bash
 
 read -p "Enter the directory: " dir
@@ -17,3 +16,7 @@ read total used <<< $(free -m | awk '/^Mem:/ {print $2, $3}')
 echo "Memory usage:"
 echo "Total Memory: ${total}MB"
 echo "Used Memory:  ${used}MB"
+
+# Find all .log files larger than 1MB
+echo "Log files larger than 1MB in $dir:"
+find "$dir" -type f -name "*.log" -size +1M -exec ls -lh {} \; | awk '{print $9, $5}'
